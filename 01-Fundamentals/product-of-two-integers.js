9181ffc8c43f8c38dc05aa51fef9b2b9720914c8
vishakha-shete <vishakhashete2@gmail.com>
2026-01-14 21:58:57 +0530

    solve que on product of two integers

---
+++ 01-Fundamentals/product-of-two-integers.js
@@ -0,0 +1,24 @@
+// ✅ Q3. Product of two integers
+
+// 1)
+let a = 4;
+let b = 5;
+console.log(4*5);
+
+//output : 20
+
+// 2
+let c = 6;
+let d = 6;
+product=6*6;
+console.log(product);
+
+//output : 36
+
+
+// 3
+let e = 10;
+let f = 5;
+console.log(e*f);
+
+//output : 50
