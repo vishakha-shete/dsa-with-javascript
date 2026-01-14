3b5999b4a6a69dee49a1e5cc574a0d604809b182
vishakha-shete <vishakhashete2@gmail.com>
2026-01-14 22:56:29 +0530

    practice Swap of two numbers

---
+++ 01-Fundamentals/swap-two-numbers.js
@@ -0,0 +1,29 @@
+// ✅ Q6. Swap two numbers (without third variable)
+
+// 1)
+ let a = 10;
+ let b = 20;
+
+ a = a + b;
+ b = a - b;
+ a = a - b;
+
+ console.log(a,b);
+
+// output : 10 20
+
+// 2)
+
+let c = 30;
+let d = 50;
+
+c = c + d;
+d = c - d;
+c = c - d;
+
+console.log(c,d);
+
+// output : 50 30
+
+
+
