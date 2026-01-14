35b3635958d9868bfce0930bc8698f17f6580fe3
vishakha-shete <vishakhashete2@gmail.com>
2026-01-14 23:04:15 +0530

    practice Find square of a number

---
+++ 01-Fundamentals/remainder-of-two-integers.js
@@ -0,0 +1,24 @@
+// ✅ Q5. Remainder of two integers
+
+// 1)
+let a = 10;
+let b = 5;
+console.log(a%b);
+
+//output : 0
+
+// 2)
+let c = 5;
+let d = 2;
+const rem = c%d;
+console.log(rem);
+
+// output : 1
+
+
+// 3)
+let e = 6;
+let f = 2;
+console.log(e%f);
+
+//output : 0
