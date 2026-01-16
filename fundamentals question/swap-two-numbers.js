d08c10d9757634ac39bf1c99fc5947ba0f185a27
vishakha-shete <vishakhashete2@gmail.com>
2026-01-16 23:11:12 +0530

    Added program to swap two numbers without using third variable

---
+++ fundamentals question/swap-two-numbers.js
@@ -0,0 +1,31 @@
+// 1
+
+function swapNumbers(a, b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+  
+    console.log(a, b);
+  }
+  
+  swapNumbers(10, 20);
+
+//   output : 20 10
+  
+
+//   2
+
+function swapNumbers1(a, b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+  
+    console.log(a, b);
+  }
+  
+  swapNumbers1(0, 0);
+
+//   output : 0 0
+
+  
+  
