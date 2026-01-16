0dfd48b7a2060d94d6f51ee9346881f74555f518
vishakha-shete <vishakhashete2@gmail.com>
2026-01-16 23:16:07 +0530

    Added greet user function using parameters

---
+++ fundamentals question/greet-user.js
@@ -0,0 +1,26 @@
+// 1
+
+function greet(name, age) {
+    console.log('Hello ' + name + ', you are ' + age + ' years old.');
+  }
+  
+  greet('Shery', 12);
+    
+// output : Hello Shery, you are 12 years old.
+
+
+
+// 2
+
+function greet(name, age) {
+    console.log('Hello ' + name + ', you are ' + age + ' years old.');
+  }
+  
+  greet('Alice', 1);
+    
+// output : Hello Alice, you are 1 years old.
+
+
+
+
+
