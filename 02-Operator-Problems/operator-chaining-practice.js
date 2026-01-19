861db3dedf2c1d10657f0883c1b568c3788eceb1
vishakha-shete <vishakhashete2@gmail.com>
2026-01-19 23:11:58 +0530

    Practiced operator chaining and assignment expressions

---
+++ 02-Operator-Problems/operator-chaining-practice.js
@@ -0,0 +1,74 @@
+// sample que
+let n = 7;
+n += 2;
+n *=3;
+n -=4;
+n %=5 ;
+console.log(n);
+//3
+
+// Q1
+let x = 5;
+x += 3;
+x *= 2;
+x -= 4;
+console.log(x);
+// Output: 12
+
+
+// Q2
+let y = 10;
+y *= 2;
+y += 5;
+y %= 6;
+console.log(y);
+// Output: 1
+
+
+// Q3
+let a = 8;
+a += 2;
+a /= 5;
+a *= 10;
+console.log(a);
+// Output: 20
+
+
+// Q4
+let b = 20;
+b -= 5;
+b /= 3;
+b += 7;
+console.log(b);
+// Output: 12
+
+
+// Q5
+let c = 7;
+c *= 3;
+c -= 5;
+c %= 4;
+console.log(c);
+// Output: 0
+
+
+// Q6
+let d = 4;
+d += d++ + ++d;
+console.log(d);
+// Output: 14
+
+
+// Q7
+let e = 6;
+e *= ++e;
+e %= 5;
+console.log(e);
+// Output: 2
+
+
+// Q8
+let f = 3;
+f += ++f * 2;
+console.log(f);
+// Output: 11
