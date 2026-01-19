5930d12ffa57def8a1531ef29f8b9150be6427c0
vishakha-shete <vishakhashete2@gmail.com>
2026-01-19 22:27:29 +0530

    Practiced find-the-value questions using operator precedence

---
+++ 02-Operator-Problems/find-the-value-operators.js
@@ -0,0 +1,49 @@
+// Q1
+let x = 50;
+x /= 5 - 2;
+console.log(x);
+// Output: 16.666666666666668
+
+
+// Q2
+let a = 24;
+a /= 4 + 2;
+console.log(a);
+// Output: 4
+
+
+// Q3
+let n = 10;
+let m = n++ + n++ + ++n;
+console.log(n, m);
+// Output: 13 34
+
+
+// Q4
+let b = 8;
+b *= 2 + 3 * 2;
+console.log(b);
+// Output: 64
+
+
+// Q5
+let c = 30;
+c -= 10 / 2;
+console.log(c);
+// Output: 25
+
+
+// Q6
+let f = 50;
+f /= 5 + 5 / 5;
+console.log(f);
+// Output: 8.333333333333334
+
+
+// Q7
+let d = 5;
+let e = ++d * 2 + d++;
+console.log(d, e);
+// Output: 7 18
+
+
