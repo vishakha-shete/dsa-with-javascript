c015bdef54ced49a01b466e3ef5cf1ca81d978a9
vishakha-shete <vishakhashete2@gmail.com>
2026-01-20 23:18:03 +0530

    Practiced predict-the-output questions using operator precedence

---
+++ 02-Operator-Problems/predict-the-output-operators.js
@@ -0,0 +1,61 @@
+// Q1
+let x = 6;
+x += 4 * 2;
+console.log(x);
+// Output: 14
+
+
+// Q2
+let a = 15;
+a -= 3 + 2 * 4;
+console.log(a);
+// Output: 4
+
+
+// Q3
+let b = 5;
+b *= 2 + 3;
+console.log(b);
+// Output: 25
+
+
+// Q4
+let c = 20;
+c /= 5 - 1;
+console.log(c);
+// Output: 5
+
+
+// Q5
+let d = 7;
+d += d++ * 2;
+console.log(d);
+// Output: 21
+
+
+// Q6
+let e = 4;
+e *= ++e + 1;
+console.log(e);
+// Output: 24
+
+
+// Q7
+let f = 10;
+f -= f-- + 2;
+console.log(f);
+// Output: -2
+
+
+// Q8
+let g = 3;
+g += ++g + g++;
+console.log(g);
+// Output: 11
+
+
+// Q9
+let y = 4;
+y += 2 * 3;
+console.log(y);
+// Output: 10
