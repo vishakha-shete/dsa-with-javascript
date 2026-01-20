c683d41455fc911fa01760f273958a2a742741b2
vishakha-shete <vishakhashete2@gmail.com>
2026-01-20 23:48:46 +0530

    Practiced division assignment operator with precedence examples

---
+++ 02-Operator-Problems/division-assignment-practice.js
@@ -0,0 +1,55 @@
+// Q1
+let x = 36;
+x /= 6;
+console.log(x);
+// Output: 6
+
+
+// Q2
+let a = 50;
+a /= 4;
+console.log(a);
+// Output: 12.5
+
+
+// Q3
+let b = 45;
+b /= 2 + 1;
+console.log(b);
+// Output: 15
+
+
+// Q4
+let c = 100;
+c /= 3 + 2 * 4;
+console.log(c);
+// Output: 9.090909090909092
+
+
+// Q5
+let d = 30;
+d /= 5 - 2;
+console.log(d);
+// Output: 10
+
+
+// Q6
+let e = 25;
+e /= 4;
+console.log(e);
+// Output: 6.25
+
+
+// Q7
+let f = 48;
+f /= 2;
+f /= 3;
+console.log(f);
+// Output: 8
+
+
+// Q8
+let g = 20;
+g /= ++g;
+console.log(g);
+// Output: 0.9523809523809523
