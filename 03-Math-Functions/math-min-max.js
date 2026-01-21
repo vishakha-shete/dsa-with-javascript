40887432c2dd55ca29a745e83bf06d690caec517
vishakha-shete <vishakhashete2@gmail.com>
2026-01-21 23:09:02 +0530

    DSA: Math.min() and Math.max() practice

---
+++ 03-Math-Functions/math-min-max.js
@@ -0,0 +1,28 @@
+// Math.max() examples
+console.log(Math.max(10, 20));            
+// 20
+console.log(Math.max(3, 7, 2, 9));         
+// 9
+console.log(Math.max(-5, -2, -10));        
+// -2
+console.log(Math.max(5));                  
+// 5
+
+
+// Math.min() examples
+console.log(Math.min(10, 20));             
+// 10
+console.log(Math.min(3, 7, 2, 9));          
+// 2
+console.log(Math.min(-5, -2, -10));         
+// -10
+console.log(Math.min(5));                  
+// 5
+
+
+let a = 15;
+let b = 8;
+let c = 20;
+
+console.log(Math.max(a, b, c));             // 20
+console.log(Math.min(a, b, c));             // 8
