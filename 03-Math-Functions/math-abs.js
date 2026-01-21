eef82a107c2802715637c7c005e60c2e596538d1
vishakha-shete <vishakhashete2@gmail.com>
2026-01-21 19:25:38 +0530

    DSA: Math.abs() practice

---
+++ 03-Math-Functions/math-abs.js
@@ -0,0 +1,39 @@
+// 1
+console.log(Math.abs(-10));
+//10
+
+console.log(Math.abs(40));
+//40
+
+console.log(Math.abs(-15));
+//15
+
+console.log(Math.abs(0));
+//0
+
+console.log(Math.abs(25));
+//25
+
+let a = 10;
+let b = 25;
+console.log(Math.abs(a-b));
+//15
+
+let x = -5;
+let y = -10;
+console.log(Math.abs(x+y));
+//15
+
+let p = 7;
+let q = 3;
+console.log(Math.abs(p-q));
+//4
+
+let a1 = -10;
+let b1 = 5;
+console.log(Math.abs(a1)-Math.abs(b1));
+//5
+
+console.log(Math.abs(-3*4+2));
+//10
+
---
+++ 03-Math-Functions/notes.md
@@ -0,0 +1,16 @@
+# Math functions and problem:
+
+--Math.abs()
+- abs is stands for absulute
+- it converts negative valur into posive value
+--Math.cell()
+--Math.floor()
+--Math.round()
+--Math.trunc()
+--Math.pow()
+--Math.sqrt()
+--Math.cbrt()
+--Math.max()
+--Math.min()
+--Math.random()
+--Math.fixed()
