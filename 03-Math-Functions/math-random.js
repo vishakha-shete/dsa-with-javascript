b8a6afee2b75a3820508ee14646422f26fea60bf
vishakha-shete <vishakhashete2@gmail.com>
2026-01-22 23:08:57 +0530

    Learned Math.random() function with practice examples

---
+++ 03-Math-Functions/math-random.js
@@ -0,0 +1,17 @@
+console.log(Math.floor(Math.random() * 11));
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// Q1
+// Generate a random number between 0 and 1
+console.log(Math.random() * 10);
+
+// Q2
+// Generate a random integer between 1 and 6 (dice)
+console.log(Math.floor(Math.random() * 6) + 1);
+
+// Q4
+// Generate a random integer between -5 and 5
+let min = -5;
+let max = 15;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
