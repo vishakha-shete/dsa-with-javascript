a1d5cde0494e8f4641daeab9bed5f9b533c95667
vishakha-shete <vishakhashete2@gmail.com>
2026-01-23 23:46:35 +0530

    Implemented compound interest calculation using Math.pow()

---
+++ 03-Math-problems/compound-interest.js
@@ -0,0 +1,23 @@
+// Compound Interest Calculation
+// Formula: CI = P * (1 + r / n)^(n * t) - P
+
+function calculateCompoundInterest(p, r, t, n) {
+    const amount = p * Math.pow(1 + r / n, n * t);
+    const compoundInterest = amount - p;
+  
+    return compoundInterest.toFixed(2);
+  }
+  
+  // Example usage (for VS Code testing)
+  let principal = 1000;
+  let rate = 0.5;   // already in decimal
+  let time = 10;    // years
+  let times = 4;    // compounded per year
+  
+  console.log(
+    "Compound Interest =",
+    calculateCompoundInterest(principal, rate, time, times)
+  );
+  
+  
+  output : 110199.00
