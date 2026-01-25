119a5305624326ecce97ecf6721c9d2b12eaed19
vishakha-shete <vishakhashete2@gmail.com>
2026-01-25 21:45:31 +0530

    Circle area and circumference problem solved

---
+++ 03-Math-problems/circleProperties.js
@@ -0,0 +1,14 @@
+// Calculate Circumference and Area of a Circle
+
+const radius = 5.2;  
+
+const circumference = 2 * Math.PI * radius;
+const area = Math.PI * radius * radius;
+
+console.log("Radius:", radius);
+console.log("Circumference:", circumference.toFixed(2));
+console.log("Area:", area.toFixed(2));
+
+output : Radius: 5.2
+         Circumference: 32.67
+         Area: 84.95
