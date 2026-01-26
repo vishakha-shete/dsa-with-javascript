d7e321a2a4e408946028606d778faa38f17f627e
vishakha-shete <vishakhashete2@gmail.com>
2026-01-26 23:22:09 +0530

    Day 14: Implemented Math.trunc() examples

---
+++ 03-Math-problems/mathTruncPractice.js
@@ -0,0 +1,11 @@
+// Day 14 - Math.trunc() Practice
+
+const num = 4.9;
+const result = Math.trunc(num);
+
+console.log("Original number:", num);
+console.log("After Math.trunc():", result);
+
+
+// output : Original number: 4.9
+//          After Math.trunc(): 4
