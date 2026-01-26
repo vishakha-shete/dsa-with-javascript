284f428525c881ee127a72024549ea3e5304d3d8
vishakha-shete <vishakhashete2@gmail.com>
2026-01-26 22:50:18 +0530

    Day 14: Implemented Math.ceil() examples

---
+++ 03-Math-problems/mathCeilPractice.js
@@ -0,0 +1,14 @@
+// Day 14 - Math.ceil() Practice
+
+const num1 = 4.2;
+const num2 = 7.8;
+const num3 = -3.4;
+
+console.log("Original:", num1, "→ Ceil:", Math.ceil(num1));
+console.log("Original:", num2, "→ Ceil:", Math.ceil(num2));
+console.log("Original:", num3, "→ Ceil:", Math.ceil(num3));
+
+
+// output: Original: 4.2 → Ceil: 5
+//         Original: 7.8 → Ceil: 8
+//         Original: -3.4 → Ceil: -3
