4c6da9d0d641a35311a0db3982c94173e072c209
vishakha-shete <vishakhashete2@gmail.com>
2026-01-26 22:43:52 +0530

    Predict the output math.min() for comparison

---
+++ 03-Math-problems/findMaximumNumber.js
@@ -0,0 +1,8 @@
+// Day 14 - Math.max() Practice
+
+const result = Math.max(-10, -5);
+
+console.log("Numbers: -10, -5");
+console.log("Largest number is:", result);
+
+// ouput : Largest number is: -5
---
+++ 03-Math-problems/findMinimumNumber.js
@@ -0,0 +1,9 @@
+// Day 14 - Math.min() Practice
+
+const result = Math.min(2, 0, -3, 5, -7);
+
+console.log("Numbers: 2, 0, -3, 5, -7");
+console.log("Smallest number is:", result);
+
+    // output : Numbers: 2, 0, -3, 5, -7
+    // Smallest number is: -7
