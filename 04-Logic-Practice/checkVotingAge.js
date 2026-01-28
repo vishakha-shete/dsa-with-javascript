7f0e8db0979306db0d8c8f82738f2bf4b3803f24
vishakha-shete <vishakhashete2@gmail.com>
2026-01-28 23:45:05 +0530

    practice if else checking age eligibility for vote problem

---
+++ 04-Logic-Practice/checkVotingAge.js
@@ -0,0 +1,12 @@
+function checkVotingAge(age) {
+  if (age >= 18) {
+    console.log("Eligible to vote");
+  } else {
+    console.log("Not eligible to vote");
+  }
+}
+
+checkVotingAge(20);
+
+
+// otput : Eligible to vote
