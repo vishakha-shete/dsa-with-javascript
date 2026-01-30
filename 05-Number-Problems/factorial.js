530f0dc7b39b15e72b92149ca782b53d21532143
vishakha-shete <vishakhashete2@gmail.com>
2026-01-30 23:38:25 +0530

    Day 18: Solved factorial problem using loop

---
+++ 05-Number-Problems/factorial.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+    let result = 1;
+
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+
+    console.log("Factorial:", result);
+}
+
+factorial(5);
+
+// output: Factorial: 120
