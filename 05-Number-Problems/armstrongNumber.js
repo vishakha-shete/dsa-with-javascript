b903b70236e32445870771ce9c27429f59047389
vishakha-shete <vishakhashete2@gmail.com>
2026-01-31 22:00:44 +0530

    Day 19: Solved Armstrong number problem

---
+++ 05-Number-Problems/armstrongNumber.js
@@ -0,0 +1,21 @@
+function isArmstrong(n) {
+    let original = n;
+    let sum = 0;
+
+    while (n > 0) {
+        let digit = n % 10;
+        sum += digit * digit * digit;
+        n = Math.floor(n / 10);
+    }
+
+    if (sum === original) {
+        console.log("Armstrong Number");
+    } else {
+        console.log("Not an Armstrong Number");
+    }
+}
+
+isArmstrong(153);
+
+// OUTPUT : Armstrong Number
+
