94c07e56e0142bbd279de065837cbc6b676387a5
vishakha-shete <vishakhashete2@gmail.com>
2026-02-07 23:35:42 +0530

    Day 25: Implemented even or odd check using conditional statements

---
+++ 06-Conditional-Statements/checkEvenOrOdd.js
@@ -0,0 +1,26 @@
+// Q. 2) check if number is Even or Odd
+
+// 1)
+
+let num = 4;
+
+if (num % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("odd");
+}
+
+//output : Even
+
+
+// 2)
+
+let num1 = 7;
+
+if (num1 % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+// output : Odd
