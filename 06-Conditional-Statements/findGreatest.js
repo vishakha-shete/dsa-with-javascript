74b42192eb609c52df08a6b7ab87c176cbbb8ee9
vishakha-shete <vishakhashete2@gmail.com>
2026-02-07 23:14:47 +0530

    Day 25: Implemented program to find greatest of two numbers using if-else

---
+++ 06-Conditional-Statements/findGreatest.js
@@ -0,0 +1,28 @@
+// Q . 1) find the greatest of two number
+
+// 1)
+
+let a = 5;
+let b = 10;
+
+if (a > b){
+    console.log(`${a} is the greatest number`);
+}else{
+    console.log(`${b} is the greatest number`);
+}
+
+// output : 10 is the greatest number
+
+
+// 2)
+
+let c = 20;
+let d = 2;
+
+if (c > d){
+    console.log(c);
+}else{
+    console.log(d);
+}
+
+// output : 20
