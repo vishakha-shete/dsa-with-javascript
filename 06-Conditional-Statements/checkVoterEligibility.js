2ce9835d20348d6b15e793ecb98de7884b4a53e6
vishakha-shete <vishakhashete2@gmail.com>
2026-02-08 00:00:02 +0530

    Day 25: Implemented voter eligibility check using conditional statements

---
+++ 06-Conditional-Statements/checkVoterEligibility.js
@@ -0,0 +1,22 @@
+// Q.3) check if user is valid voter
+
+let age = 25;
+
+if ( age >= 18){
+    console.log(`you are ${age} years old thats why your eligible for voting`);
+}else{
+    console.log(`you are ${age} years old your not eligible`);
+}
+
+// output : you are 25 years old thats why your eligible for voting
+
+
+
+let age1 =  12;
+if ( age1 >= 18 ){
+    console.log( ` age : ${age1} it is a valid voter`);
+}else{
+    console.log(`age : ${age1} it is not valid voter`);
+}
+
+// output : age : 12 it is not valid voter
