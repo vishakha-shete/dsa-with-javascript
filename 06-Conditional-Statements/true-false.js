54783c00f58fc9f14e705171e6d609e5740676a8
vishakha-shete <vishakhashete2@gmail.com>
2026-02-11 23:27:04 +0530

    day 29 :Q.2) accept an integer and check whether it s true or false

---
+++ 06-Conditional-Statements/true-false.js
@@ -0,0 +1,11 @@
+// Q.2) accept an integer and check whether it s true or false
+
+let num = Number(prompt("Enter a number"))
+
+if (num % 2 == 0){
+    console.log("given number is Even number");
+}else{
+    console.log("given number is Odd number");
+}
+
+// output : Even
