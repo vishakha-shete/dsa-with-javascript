9817b0cfc79de10d143e6399e516a611cb512662
vishakha-shete <vishakhashete2@gmail.com>
2026-02-12 16:37:23 +0530

    day-30: second approach

--- 06-Conditional-Statements/shop-discount.js
+++ 06-Conditional-Statements/shop-discount.js
@@ -14,6 +14,7 @@ const prompt = require("prompt-sync")();
 let amount = Number(prompt("Enter the amount of shopping"));
 
 payableAmount = 0;
+
 if (amount > 0 && amount <= 5000) {
     payableAmount = amount;
 } else if (amount > 5001 && amount <= 7000) {
@@ -26,3 +27,17 @@ else if (amount > 7001 && amount <= 9000) {
 }
 
 console.log(`with discount your payable-amount is : ${payableAmount}`);
+
+
+// 2nd approach {convert code into easy way}
+
+let Amount = Number(prompt("Enter Amount"));
+
+let dis = 0;
+
+if (Amount>0 && Amount<=5000)dis = 0;
+else if (Amount>5001 && Amount<=7000) dis = 5;
+else if(Amount>7001 && Amount<9000) dis = 10;
+else dis= 20;
+
+console.log("payable-Amount : " +(Amount-(dis*Amount)/100));
