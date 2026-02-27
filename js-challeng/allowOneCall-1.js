f164ddc59f144bc822895e42eabac11bc3ccf322
vishakha-shete <vishakhashete2@gmail.com>
2026-02-27 22:28:58 +0530

    Day 40: Solve LeetCode 2666 - Allow One Function Call

--- js-challeng/allowOneCall-1.js
+++ js-challeng/allowOneCall-1.js
@@ -17,6 +17,7 @@ const onceAdd = once(add);
 console.log(onceAdd(1, 2, 3)); // 6
 console.log(onceAdd(2, 3, 6)); // undefined
 
+
 // Example 2
 const multiply = (a, b, c) => a * b * c;
 const onceMultiply = once(multiply);
