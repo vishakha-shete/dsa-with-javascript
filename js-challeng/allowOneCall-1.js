80cb1c7777407408453a6c7ac9aabe71f04b9ef6
vishakha-shete <vishakhashete2@gmail.com>
2026-02-27 22:26:44 +0530

    Implement once function using closure to restrict execution

---
+++ js-challeng/allowOneCall-1.js
@@ -0,0 +1,25 @@
+function once(fn) {
+    let called = false;
+
+    return function (...args) {
+        if (!called) {
+            called = true;
+            return fn(...args);
+        }
+        return undefined;
+    };
+}
+
+// Example 1
+const add = (a, b, c) => a + b + c;
+const onceAdd = once(add);
+
+console.log(onceAdd(1, 2, 3)); // 6
+console.log(onceAdd(2, 3, 6)); // undefined
+
+// Example 2
+const multiply = (a, b, c) => a * b * c;
+const onceMultiply = once(multiply);
+
+console.log(onceMultiply(5, 7, 4)); // 140
+console.log(onceMultiply(2, 3, 6)); // undefined
